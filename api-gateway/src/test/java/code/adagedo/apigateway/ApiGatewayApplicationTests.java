29268881f52cc172badcacf5fa48db7d56f567a6
Adagedo solomon Israel <adagedosolomon52@gmail.com>
2026-06-29 14:03:55 +0100

    added api gate way service

---
+++ api-gateway/src/test/java/code/adagedo/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package code.adagedo.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
