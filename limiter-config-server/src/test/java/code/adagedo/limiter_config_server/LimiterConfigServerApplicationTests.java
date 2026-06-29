a65f3caa601c52499186f9bdf46636270550dbd6
Adagedo solomon Israel <adagedosolomon52@gmail.com>
2026-06-29 13:30:54 +0100

    initial commit

---
+++ limiter-config-server/src/test/java/code/adagedo/limiter_config_server/LimiterConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package code.adagedo.limiter_config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimiterConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
