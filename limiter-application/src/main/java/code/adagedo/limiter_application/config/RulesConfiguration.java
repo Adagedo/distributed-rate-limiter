e5fde60e7565ca6858b3b27996cfc66823efa5b7
Adagedo solomon Israel <adagedosolomon52@gmail.com>
2026-06-29 13:57:02 +0100

    added configs

---
+++ limiter-application/src/main/java/code/adagedo/limiter_application/config/RulesConfiguration.java
@@ -0,0 +1,9 @@
+package code.adagedo.limiter_application.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties
+public class RulesConfiguration {
+}
