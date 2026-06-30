50425ae80397e13374bee8d759b4d655e4b24d94
Adagedo solomon Israel <adagedosolomon52@gmail.com>
2026-06-30 11:59:45 +0100

    modified and added mvn modules

---
+++ limit-proto/src/main/java/code/adagedo/limitproto/LimitProtoApplication.java
@@ -0,0 +1,13 @@
+package code.adagedo.limitproto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimitProtoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LimitProtoApplication.class, args);
+    }
+
+}
---
+++ limit-proto/src/test/java/code/adagedo/limitproto/LimitProtoApplicationTests.java
@@ -0,0 +1,13 @@
+package code.adagedo.limitproto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitProtoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
